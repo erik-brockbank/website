350840900c39a13a0f47814577331acb703b0c9a
Erik Brockbank <erikb@erikb-mbp.corp.dropbox.com>
2017-02-13 19:31:38 -0800

    Progress

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
+
+@app.route("/cv")
+def cv():
+    return "CV page"
+
+#TO RUN THIS
+# python main.py
+# -> Running on http://localhost:5000/
 
 if __name__ == "__main__":
     app.run()
