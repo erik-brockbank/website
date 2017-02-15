3b978c600805bd537c770cb1f51ab0f0afc8085a
Erik Brockbank <erikb@erikb-mbp.corp.dropbox.com>
2017-02-14 16:20:42 -0800

    bleh

--- main.py
+++ main.py
@@ -15,4 +15,5 @@ def cv():
 
 if __name__ == "__main__":
     from os import environ
-    app.run(debug=False, port=environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 33507))
+    app.run(debug=False, host='0.0.0.0', port=port)
