612cce690dd34141c166c7ac7b1094826c56799d
Erik Brockbank <erikbrockbank@eriks-mbp.attlocal.net>
2021-08-21 12:12:27 -0700

    another attempt

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def contact():
 # -> Running on http://localhost:33507/
 
 if __name__ == "__main__":
-    # from os import environ
-    # port = int(os.environ.get("PORT", 33507))
-    # app.run(debug=False, host="0.0.0.0", port=port)
-    app.run()
+    from os import environ
+    port = int(environ.get("PORT", 33507))
+    app.run(debug=False, host="0.0.0.0", port=port)
